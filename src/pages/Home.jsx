d54e5690137cb2ceb1c1702536fb512557db2722
Muhammad Daniyal <daniyal.amjad7989@gmail.com>
2026-03-29 08:32:55 +0500

    Comment out TestimonialsSection in Home component
    Comment out the TestimonialsSection component in Home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import TechStack from '../components/home/TechStack';
 import ServicesPreview from '../components/home/ServicesPreview';
 import AboutPreview from '../components/home/AboutPreview';
 import ProjectsPreview from '../components/home/ProjectsPreview';
-import TestimonialsSection from '../components/home/TestimonialsSection';
+//import TestimonialsSection from '../components/home/TestimonialsSection';
 import CTASection from '../components/home/CTASection';
 
 export default function Home() {
@@ -15,7 +15,7 @@ export default function Home() {
       <ServicesPreview />
       <AboutPreview />
       <ProjectsPreview />
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       <CTASection />
     </>
   );
